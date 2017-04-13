a62f99f6f69d568529099afcacf1a2d15eb1cffd
Rory Devane <rory.devane@gmail.com>
2017-04-13 11:49:19 +0100

    added friend input and friend list components

---
+++ src/components/AddFriendInput.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class AddFriendInput extends Component {
+    render(){
+        return (
+            <div>Input</div>
+        )
+    }
+}
